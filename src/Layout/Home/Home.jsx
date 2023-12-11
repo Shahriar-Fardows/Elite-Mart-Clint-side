02ec81dda872af521bfc4b7a1e94d6ef4facc56e
Sharmin Akterr <sharminakter3e@gmail.com>
2023-12-11 20:15:36 +0600

    home pic layout fixed

--- src/Layout/Home/Home.jsx
+++ src/Layout/Home/Home.jsx
@@ -2,14 +2,14 @@
 const Home = () => {
     return (
         <div>
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
                 <div >
-                    <span className="grid gap-4">
+                    <span className="grid grid-cols-1 gap-4">
                         <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="" />
                         <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="" />
                     </span>
                 </div>
-                <div className="md:col-span-2">
+                <div className="col-span-1 md:col-span-2">
                     <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="" />
                 </div>
                 <div>
